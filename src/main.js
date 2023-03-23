dea792cad953aa52bbfc9809036e5a1fff9ad756
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2023-03-23 13:58:04 +0100

    Adição do sass ao projecto

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js'
-import './assets/main.css'
+import './assets/scss/main.scss'
 
 const app = createApp(App)
 
