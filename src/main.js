123ad484675f36cc16468fdc0ce2cc1c3144fb75
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2023-03-23 13:47:19 +0100

    Adicionei o bootstrap ao projecto

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js'
 import './assets/main.css'
 
 const app = createApp(App)
