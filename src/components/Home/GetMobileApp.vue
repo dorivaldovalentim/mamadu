3c6278979839292fc0a2fbb0af8a2b9779119f10
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2023-04-07 19:43:37 +0100

    Trabalhei na responsividade da secção getMobileApp

--- src/components/Home/GetMobileApp.vue
+++ src/components/Home/GetMobileApp.vue
@@ -5,12 +5,16 @@
     <div class="container">
       <div class="row align-items-stretch">
         <div class="col-12 col-md-6 col-lg-6 get-mobile-app-description">
-          <h2 class="display-4 fw-bold text-white">
+          <h2
+            class="display-4 fw-bold text-white text-center text-md-start text-lg-start"
+          >
             Compre <span class="text-secondary">qualquer</span> <br />
             <span class="text-secondary">coisa</span> perto de si
           </h2>
 
-          <div class="d-flex">
+          <div
+            class="d-flex justify-content-center justify-content-md-start justify-content-lg-start"
+          >
             <a href=""
               ><img src="/src/assets/images/google-play.png" alt=""
             /></a>
@@ -24,7 +28,7 @@
           <img
             src="/src/assets/images/get-mobile-app.png"
             alt="Mobile apps"
-            class="phones-image position-absolute"
+            class="phones-image"
           />
         </div>
       </div>
@@ -33,13 +37,27 @@
 </template>
 
 <style lang="scss" scoped>
+@import "@/assets/scss/responsive";
+
 .get-mobile-app-description {
   padding-block: 120px;
+
+  @include responsive(extra-small, small) {
+    padding-block: 30px 60px;
+    order: 2;
+  }
 }
 .phones-image {
   width: 650px;
   height: auto;
   top: -100px;
   right: 0;
+  position: absolute;
+
+  @include responsive(extra-small, small) {
+    position: relative;
+    width: 100%;
+    top: 0;
+  }
 }
 </style>
