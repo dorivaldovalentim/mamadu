6f03e890108b0544776d660fea35372f07a201e7
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2023-04-07 15:59:27 +0100

    Fiz algumas melhorias no footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,82 +1,78 @@
 <script setup>
+import Logo from "./Logo.vue";
+
 const socialNetworks = [
   { name: "LinkedIn", link: "https://linkedin.com", icon: "bi-linkedin" },
   { name: "Instagram", link: "https://instagram.com", icon: "bi-instagram" },
   { name: "Twitter", link: "https://twitter.com", icon: "bi-twitter" },
   { name: "Facebook", link: "https://facebook.com", icon: "bi-facebook" },
 ];
+
+const data = [
+  {
+    title: "Suporte",
+    links: [
+      { label: "Conta", link: "#" },
+      { label: "Centro de ajuda", link: "#" },
+      { label: "Feedbacks", link: "#" },
+      { label: "Acessibilidade", link: "#" },
+    ],
+  },
+  {
+    title: "Links úteis",
+    links: [
+      { label: "Pagamentos e taxas", link: "#" },
+      { label: "Termos de serviço", link: "#" },
+      { label: "Política de Privacidade", link: "#" },
+      { label: "Sobre nós", link: "#" },
+    ],
+  },
+  {
+    title: "Nosso menu",
+    links: [
+      { label: "Mais vendidos", link: "#" },
+      { label: "Categorias", link: "#" },
+    ],
+  },
+];
 </script>
 
 <template>
-  <footer class="pt-5">
+  <footer>
     <div class="container">
       <div class="row justify-content-between">
         <div class="col-12 col-md-4 col-lg-4">
-          <a href="" class="text-decoration-none">
-            <h2 class="h2 fw-bold text-uppercase">
-              <span class="text-primary">Mam</span
-              ><span class="text-secondary">adu</span>
-            </h2>
+          <a href="" class="text-decoration-none d-inline-block w-25">
+            <Logo />
           </a>
-          <p class="fs-5 fw-bold">
+
+          <p class="fs-5 pe-2 mt-4 text-primary">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
             labore.
           </p>
         </div>
-        <div class="col-12 col-md-3 col-lg-2">
-          <h3 class="h4 fw-bold text-primary">Title</h3>
-          <ul class="list-unstyled">
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 1</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 2</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 3</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 4</a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-12 col-md-3 col-lg-2">
-          <h3 class="h4 fw-bold text-primary">Title</h3>
-          <ul class="list-unstyled">
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 1</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 2</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 3</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 4</a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-12 col-md-3 col-lg-2">
-          <h3 class="h4 fw-bold text-primary">Title</h3>
-          <ul class="list-unstyled">
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 1</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 2</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 3</a>
-            </li>
-            <li class="mb-1">
-              <a href="" class="text-primary text-decoration-none">Link 4</a>
+        <div
+          class="col-12 col-md-3 col-lg-2"
+          v-for="(item, index) in data"
+          :key="index"
+        >
+          <h3 class="h4 fw-bold text-primary mb-4">{{ item.title }}</h3>
+
+          <ul class="list-unstyled" v-if="item.links && item.links.length">
+            <li
+              class="mb-4"
+              v-for="(subItem, subIndex) in item.links"
+              :key="subIndex"
+            >
+              <a :href="subItem.link" class="text-decoration-none">{{
+                subItem.label
+              }}</a>
             </li>
           </ul>
         </div>
       </div>
 
-      <div class="border-bottom border-3">
+      <div class="border-bottom border-3 mt-3">
         <ul class="list-unstyled d-flex">
           <li v-for="(item, index) in socialNetworks" :key="index">
             <a
@@ -89,10 +85,20 @@ const socialNetworks = [
         </ul>
       </div>
 
-      <p class="py-2 text-primary text-center fw-bold">
+      <p class="py-3 text-primary text-center fw-bold">
         Copyright &copy; 2022 - <span class="text-primary">MAM</span
         ><span class="text-secondary">ADU</span>
       </p>
     </div>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+footer {
+  margin-top: 150px;
+
+  a {
+    color: #9ea8c2;
+  }
+}
+</style>
